40a5306cf4f980891f71b82e22b0acabd91a6263
Mitchell-kolb <mkolb321@gmail.com>
2024-02-22 14:20:43 -0800

    Added code for GUI

--- gui_menu.py
+++ gui_menu.py
@@ -1,17 +1,30 @@
 import customtkinter
 import os
 from PIL import Image
+import subprocess
+import sys
+import webbrowser
+
+from requester import edit_credentials_gui, download_files
+from djc_barmaker import setup as djc_setup
+
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
+        # Get the current working directory (where the script is located)
+        self.current_directory = os.path.dirname(os.path.realpath(__file__))
+        # Checks/creates the Results folder in this directory
+        self.results_dir_path = os.path.join(self.current_directory, "Results")
+        self.people_dir_path = os.path.join(self.current_directory, "People")
+
         self.title("GSUR Automation")
         #self.geometry("950x450")
 
         # Calculate window position to center the GUI in the middle of the users screen dynamically
-        window_width = 700
+        window_width = 550
         window_height = 400
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
@@ -49,10 +62,10 @@ class App(customtkinter.CTk):
                                                              compound="left", font=customtkinter.CTkFont(size=18, weight="bold"))
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
 
-        self.test_selenium_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Test Selenium",
-                                                   fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                   image=self.home_image, anchor="w", command=self.test_selenium_button_event)
-        self.test_selenium_button.grid(row=1, column=0, sticky="ew")
+        # self.test_selenium_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Test Selenium",
+        #                                            fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+        #                                            image=self.home_image, anchor="w", command=self.test_selenium_button_event)
+        # self.test_selenium_button.grid(row=1, column=0, sticky="ew")
 
         self.edit_login_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Edit Login",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
@@ -84,52 +97,74 @@ class App(customtkinter.CTk):
 
 
         # create test selenium frame or RIGHT 3/4 of screen
-        self.test_selenium_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.test_selenium_frame.grid_columnconfigure(0, weight=1)
+        # self.test_selenium_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        # self.test_selenium_frame.grid_columnconfigure(0, weight=1)
 
-        self.test_selenium_frame_large_image_label = customtkinter.CTkLabel(self.test_selenium_frame, text="", image=self.large_test_image)
-        self.test_selenium_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
+        # self.test_selenium_frame_large_image_label = customtkinter.CTkLabel(self.test_selenium_frame, text="", image=self.large_test_image)
+        # self.test_selenium_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
 
-        self.test_selenium_frame_button_1 = customtkinter.CTkButton(self.test_selenium_frame, text="", image=self.image_icon_image)
-        self.test_selenium_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.test_selenium_frame_button_2 = customtkinter.CTkButton(self.test_selenium_frame, text="CTkButton1", image=self.image_icon_image, compound="right")
-        self.test_selenium_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.test_selenium_frame_button_3 = customtkinter.CTkButton(self.test_selenium_frame, text="CTkButton2", image=self.image_icon_image, compound="top")
-        self.test_selenium_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
-        self.test_selenium_frame_button_4 = customtkinter.CTkButton(self.test_selenium_frame, text="CTkButton3", image=self.image_icon_image, compound="bottom", anchor="w")
-        self.test_selenium_frame_button_4.grid(row=4, column=0, padx=20, pady=10)
+        # self.test_selenium_frame_button_1 = customtkinter.CTkButton(self.test_selenium_frame, text="", image=self.image_icon_image)
+        # self.test_selenium_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
+        # self.test_selenium_frame_button_2 = customtkinter.CTkButton(self.test_selenium_frame, text="CTkButton1", image=self.image_icon_image, compound="right")
+        # self.test_selenium_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
+        # self.test_selenium_frame_button_3 = customtkinter.CTkButton(self.test_selenium_frame, text="CTkButton2", image=self.image_icon_image, compound="top")
+        # self.test_selenium_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
+        # self.test_selenium_frame_button_4 = customtkinter.CTkButton(self.test_selenium_frame, text="CTkButton3", image=self.image_icon_image, compound="bottom", anchor="w")
+        # self.test_selenium_frame_button_4.grid(row=4, column=0, padx=20, pady=10)
 
         # create Edit login frame
         self.edit_login_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.edit_login_frame.grid_columnconfigure(0, weight=1)
-        self.edit_login_frame_button_5 = customtkinter.CTkButton(self.edit_login_frame, text="CTkButton5 ahhh", image=self.image_icon_image, compound="right")
-        self.edit_login_frame_button_5.grid(row=2, column=0, padx=20, pady=10)
+        self.edit_login_frame.grid_rowconfigure(0, weight=1)  # Add this line to make the row above your widgets expand
+        self.edit_login_frame.grid_rowconfigure(5, weight=1)  # Add this line if you want space below your widgets, adjust as necessary
+
+        self.edit_login_frame_entry_1 = customtkinter.CTkEntry(self.edit_login_frame, placeholder_text="Username", width=200)
+        self.edit_login_frame_entry_1.grid(row=1, column=0, padx=20, pady=10)
+        self.edit_login_frame_entry_2 = customtkinter.CTkEntry(self.edit_login_frame, placeholder_text="Password", show="*", width=200)
+        self.edit_login_frame_entry_2.grid(row=2, column=0, padx=20, pady=10)
+        self.edit_login_frame_button_3 = customtkinter.CTkButton(self.edit_login_frame, text="Save Credentials", command=self.on_submit_credentials, width=200)
+        self.edit_login_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
+        self.credentials_saved_label = customtkinter.CTkLabel(self.edit_login_frame, text="", width=200, text_color="green")
+
 
         # create Download frame
         self.download_files_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.download_files_frame.grid_columnconfigure(0, weight=1)
-        self.download_files_frame_button_6 = customtkinter.CTkButton(self.download_files_frame, text="CTkButton6 ahhh", image=self.image_icon_image, compound="right")
-        self.download_files_frame_button_6.grid(row=2, column=0, padx=20, pady=10)
+        self.download_files_frame.grid_rowconfigure(0, weight=1)  # Add this line to make the row above your widgets expand
+        self.download_files_frame.grid_rowconfigure(5, weight=1)  # Add this line if you want space below your widgets, adjust as necessary
+
+        self.download_files_frame_button_1 = customtkinter.CTkButton(self.download_files_frame, text="Begin Download", width=200, command=self.on_submit_download_files)
+        self.download_files_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
+        self.download_files_frame_label_1 = customtkinter.CTkLabel(self.download_files_frame, text="", width=200)
+        self.download_files_frame_button_2 = customtkinter.CTkButton(self.download_files_frame, text="Open Folder", command=lambda: self.open_folder(self.people_dir_path), width=200)
 
         # create Analzye frame
         self.analyze_files_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.analyze_files_frame.grid_columnconfigure(0, weight=1)
-        self.analyze_files_frame_button_7 = customtkinter.CTkButton(self.analyze_files_frame, text="CTkButton7 ahhh", image=self.image_icon_image, compound="right")
-        self.analyze_files_frame_button_7.grid(row=2, column=0, padx=20, pady=10)
+        self.analyze_files_frame.grid_rowconfigure(0, weight=1)  # Add this line to make the row above your widgets expand
+        self.analyze_files_frame.grid_rowconfigure(5, weight=1)  # Add this line if you want space below your widgets, adjust as necessary
+
+        self.analyze_files_frame_button_1 = customtkinter.CTkButton(self.analyze_files_frame, text="Begin Analysis", width=200, command=self.on_submit_analyze_files)
+        self.analyze_files_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
+        self.analyze_files_frame_label_1 = customtkinter.CTkLabel(self.analyze_files_frame, text="", width=200)
+        self.analyze_files_frame_button_2 = customtkinter.CTkButton(self.analyze_files_frame, text="Open Folder", command=lambda: self.open_folder(self.results_dir_path), width=200)
 
         # create Settings frame
         self.settings_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.settings_frame.grid_columnconfigure(0, weight=1)
-        self.settings_frame_button_8 = customtkinter.CTkButton(self.settings_frame, text="CTkButton8 ahhh", image=self.image_icon_image, compound="right")
-        self.settings_frame_button_8.grid(row=2, column=0, padx=20, pady=10)
+        self.settings_frame.grid_rowconfigure(0, weight=1)  # Add this line to make the row above your widgets expand
+        self.settings_frame.grid_rowconfigure(5, weight=1)  # Add this line if you want space below your widgets, adjust as necessary
+
+        self.settings_frame_button_1 = customtkinter.CTkButton(self.settings_frame, text="Link to Github", width=200, command=self.open_github)
+        self.settings_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
 
 
         # select default frame
-        self.select_frame_by_name("Test Selenium")
+        #self.select_frame_by_name("Test Selenium")
 
     def select_frame_by_name(self, name):
         # set button color for selected button
-        self.test_selenium_button.configure(fg_color=("gray75", "gray25") if name == "Test Selenium" else "transparent")
+        #self.test_selenium_button.configure(fg_color=("gray75", "gray25") if name == "Test Selenium" else "transparent")
         self.edit_login_button.configure(fg_color=("gray75", "gray25") if name == "Edit Login" else "transparent")
         self.download_files_button.configure(fg_color=("gray75", "gray25") if name == "Download Files" else "transparent")
         self.analyze_files_button.configure(fg_color=("gray75", "gray25") if name == "Analyze Files" else "transparent")
@@ -137,10 +172,10 @@ class App(customtkinter.CTk):
 
 
         # show selected frame
-        if name == "Test Selenium":
-            self.test_selenium_frame.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.test_selenium_frame.grid_forget()
+        # if name == "Test Selenium":
+        #     self.test_selenium_frame.grid(row=0, column=1, sticky="nsew")
+        # else:
+        #     self.test_selenium_frame.grid_forget()
         if name == "Edit Login":
             self.edit_login_frame.grid(row=0, column=1, sticky="nsew")
         else:
@@ -158,8 +193,8 @@ class App(customtkinter.CTk):
         else:
             self.settings_frame.grid_forget()
 
-    def test_selenium_button_event(self):
-        self.select_frame_by_name("Test Selenium")
+    # def test_selenium_button_event(self):
+    #     self.select_frame_by_name("Test Selenium")
 
     def edit_login_button_event(self):
         self.select_frame_by_name("Edit Login")
@@ -175,6 +210,45 @@ class App(customtkinter.CTk):
 
     def change_appearance_mode_event(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def on_submit_credentials(self):
+        # Retrieve text from the entry widgets
+        username = self.edit_login_frame_entry_1.get()
+        password = self.edit_login_frame_entry_2.get()
+        
+        # Call set_creds with the retrieved text
+        edit_credentials_gui(username, password)
+
+        # Update the label's text to show the confirmation message and make it visible by adding it to the grid.
+        self.credentials_saved_label.configure(text="Credentials Saved")
+        self.credentials_saved_label.grid(row=4, column=0, padx=20, pady=10)  # Adjust row, column as needed
+
+    def on_submit_download_files(self):
+        message = download_files()
+        #print(message)
+        self.download_files_frame_label_1.configure(text=message)                                    
+        self.download_files_frame_label_1.grid(row=2, column=0, padx=20, pady=10)  # Adjust row, column as needed
+        self.download_files_frame_button_2.grid(row=3, column=0, padx=20, pady=10)
+
+    def on_submit_analyze_files(self):
+        message = djc_setup()
+        self.analyze_files_frame_label_1.configure(text=message)                                    
+        self.analyze_files_frame_label_1.grid(row=2, column=0, padx=20, pady=10)  # Adjust row, column as needed
+        self.analyze_files_frame_button_2.grid(row=3, column=0, padx=20, pady=10)
+
+    def open_folder(self, path):
+        if sys.platform == "win32":
+            # Command for Windows
+            subprocess.run(["explorer", path])
+        elif sys.platform == "darwin":
+            # Command for macOS
+            subprocess.run(["open", path])
+        else:
+            print("Error")
+    
+    def open_github(self):
+        url = 'https://github.com/Mitchell-kolb/GSUR'
+        webbrowser.open(url)
 
 
 if __name__ == "__main__":
